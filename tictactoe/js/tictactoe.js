50f5033cb523601ee126f44536acb3694db76055
Marcelo Salazar <marsal01@E109747-MAC.local>
2016-12-30 19:00:16 +0100

    Added dynamic table in js

--- tictactoe/js/tictactoe.js
+++ tictactoe/js/tictactoe.js
@@ -1,3 +1,23 @@
 function NewGame() {
+
+
+    var htmlarray_id = document.getElementById("mytable");
+
+    var result = '';
+
+
+    for (var i = 0; i < 3; i++) {
+
+        result += '<tr>';
+
+        for (var j = 0; j < 3; j++) {
+            result += '<td>' + 'x' + "</td>";
+        }
+        result += "</tr>";
+    }
+
+    htmlarray_id.innerHTML = result;
+
     console.log("Hello tictactoe");
+
 }
